edc96fd92848102c02abe480e2397864ba9903b6
Yeong joon <bluehome8626@naver.com>
2023-03-23 12:30:30 +0900

    add : 프로젝트 세팅

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css");
+    * {
+        margin:0px;
+        padding:0px;
+        box-sizing:border-box;
+        font-style: normal;
+        -webkit-user-select:none;
+        -moz-user-select:none;
+        -ms-user-select:none;
+        user-select:none;
+        font-family:"Pretendard";
+        scroll-behavior: smooth;
+        background-color: #FCFCFC;
+    }    
+    html{
+        overflow-x:hidden;
+        overflow-y:scroll;
+        scroll-snap-type:y mandatory;
+        scroll-snap-stop: always;
+    }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+const colors = {
+  greanScale: {
+    main: "#AFDCAB",
+    grean: "69D2A6",
+    Dark_Grean: "#ACC7B4",
+    Light_Grean: "#6BC662",
+  },
+  grayScale: {
+    Light_Gray: "#F2F2F2",
+    Gray: "#D7D7D7",
+    Dark_Gray: "#BBBCBD",
+    Very_Dark_Gray: "#959697",
+  },
+  star: "#FFD233",
+  TextColor: "#766D6D",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const theme = {
+  colors,
+};
+export type Theme = typeof theme;
